33bf0b5faa8b98690b128a22e770321a97a1f165
k-nasa <htilcs1115@gmail.com>
2018-09-12 23:09:02 +0900

    add repositories/ ProfileQuastionElementのヘルパー概形を追加

---
+++ repositories/profile_quastion_element.go
@@ -0,0 +1,30 @@
+package repositories
+
+import (
+	"github.com/eure/si2018-second-half-3/entities"
+)
+
+type ProfileQuastionElementRepository struct {
+  RootRepository
+}
+
+func NewProfileQuastionElementRepository(s *Session) *ProfileQuastionElementRepository {
+  return &ProfileQuastionElementRepository{ NewRootRepository(s) }
+}
+
+func (r *ProfileQuastionElementRepository) Create(ent entities.ProfileQuastionElement) error {
+  s := r.GetSession()
+  if _, err := s.Insert(&ent); err != nil {
+    return err
+  }
+
+  return nil
+}
+
+func (r *ProfileQuastionElementRepository) Update(ent *entities.ProfileQuastionElement) error {
+	s := r.GetSession().Where("id = ?", ent.ID)
+	if _, err := s.Update(ent); err != nil {
+		return err
+	}
+	return nil
+}

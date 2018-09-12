e8cd7dfcded95f8883399d712a83f1249e282bda
Rentearo Igari <>
2018-09-12 18:55:38 +0900

    add foreign key to profile quastion content

--- db/migrations/20180912164638_createProfileQuastionContent.sql
+++ db/migrations/20180912164638_createProfileQuastionContent.sql
@@ -4,7 +4,8 @@
 CREATE TABLE IF NOT EXISTS `profile_quastion_content` (
   `profile_quastion_id` integer unsigned NOT NULL COMMENT 'profile_quastion ID',
   `type` varchar(255) NOT NULL COMMENT '質問内容の種類',
-  `message` text NOT NULL COMMENT '質問項目の内容'
+  `message` text NOT NULL COMMENT '質問項目の内容',
+  FOREIGN KEY (profile_quastion_id) REFERENCES  profile_quastion (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='チャット式プロフィール入力のマスターデータの詳細';
 
 

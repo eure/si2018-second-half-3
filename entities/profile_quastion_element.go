0863e4b22aa4bc97c2d150915568258d8f6f9753
k-nasa <htilcs1115@gmail.com>
2018-09-13 11:59:48 +0900

    add/ 扱いやすそうなのでentities.ProfileQuastionElementのプロパティにContentを含める

--- entities/profile_quastion_element.go
+++ entities/profile_quastion_element.go
@@ -4,4 +4,6 @@ type ProfileQuastionElement struct {
 	ID       int64  `xorm:"id"`
 	Priority int64  `xorm:"priority"`
 	Name     string `xorm:"name"`
+
+	Content ProfileQuastionContent `xorm:"-"`
 }

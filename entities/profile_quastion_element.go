b4e6fc311905890d78186cdb5ffff5b91cb84730
k-nasa <htilcs1115@gmail.com>
2018-09-13 14:08:10 +0900

    add/ entitiesをmodelsに変換するメソッドを追加

--- entities/profile_quastion_element.go
+++ entities/profile_quastion_element.go
@@ -1,9 +1,46 @@
 package entities
 
+import (
+	"github.com/eure/si2018-second-half-3/models"
+)
+
 type ProfileQuastionElement struct {
 	ID       int64  `xorm:"id"`
 	Priority int64  `xorm:"priority"`
 	Name     string `xorm:"name"`
 
-	Content ProfileQuastionContent `xorm:"-"`
+	Contents []ProfileQuastionContent `xorm:"-"`
+}
+
+func (p ProfileQuastionElement) Build() models.ProfileQuastionElement {
+	var profileQuastionContent models.ProfileQuastionContent
+
+	// 本来はentities.ProfileQuastionContentないでモデルへの変換をやるべきだけどここでやっちゃう
+	for _, content := range p.Contents {
+		contentItem := models.ProfileQuastionContentItems0{
+			Message: content.Message,
+			Type:    content.Type,
+		}
+
+		profileQuastionContent = append(profileQuastionContent, &contentItem)
+	}
+
+	return models.ProfileQuastionElement{
+		Contents: profileQuastionContent,
+		Name:     p.Name,
+		Priority: p.Priority,
+	}
+}
+
+type ProfileQuastionElements []ProfileQuastionElement
+
+func (elements *ProfileQuastionElements) Build() []*models.ProfileQuastionElement {
+	var elements_model []*models.ProfileQuastionElement
+
+	for _, e := range *elements {
+		elementModel := e.Build()
+		elements_model = append(elements_model, &elementModel)
+	}
+
+	return elements_model
 }
